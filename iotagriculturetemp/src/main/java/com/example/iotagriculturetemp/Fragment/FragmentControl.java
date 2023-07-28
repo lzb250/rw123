ac3504f87669a273b5034ec345f1ebed5a1300f2
21920 <2192027655@qq.com>
2023-07-28 13:33:09 +0800

    Changes

--- iotagriculturetemp/src/main/java/com/example/iotagriculturetemp/Fragment/FragmentControl.java
+++ iotagriculturetemp/src/main/java/com/example/iotagriculturetemp/Fragment/FragmentControl.java
@@ -5,22 +5,45 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.example.iotagriculturetemp.R;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 public class FragmentControl extends Fragment {
+
+    private TextView tv_message;
     public FragmentControl() {
         // Required empty public constructor
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EventBus.getDefault().register(this);
 
+    }
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onHomeCtrEvent(String recvData) {
+        handleSocketReciveData(recvData);
+    }
+    private void handleSocketReciveData(String recvData) {
+        tv_message.setText(recvData);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.layout_fragment_control, container, false);
+        View view = inflater.inflate(R.layout.layout_fragment_control, container, false);
+        tv_message = view.findViewById(R.id.tv_message);
+        return view;
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
 }
